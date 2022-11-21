81fa6e9fcc393866b06b41953933ff260f9fbd1c
Ahranna Araujo <ahranna@ahrannas-mbp.lan>
2022-11-21 10:16:33 +0000

    password criptografado e fazendo token para os usuários

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import dayjs from "dayjs";
+import bcrypt from "bcrypt"
+import { v4 as uuidV4 } from "uuid"
 
 const validacaoSchema = joi.object({
     email: joi.string().required().email(),
     name: joi.string().required(),
+    password: joi.string().required(),
 })
 
 const transactionSchema = joi.object({
@@ -35,36 +38,29 @@ const collectionUser = db.collection("user")
 const collectionTransaction = db.collection("transaction")
 
 app.post("/login", async (req, res) => {
-    const { email } = req.body
-    const { password } = req.headers
-    console.log(password)
-
-
-    const validation = validacaoSchema.validate({ email }, { abortEarly: false })
-
-    if (validation.error) {
-        const erros = validation.error.details.map((detail) => detail.message)
-        res.status(422).send(erros)
-        return;
-    }
+    const { email, password } = req.body
+    const token = uuidV4()
 
     try {
         const user = await collectionUser.findOne({ email })
-        console.log(user)
-
-        if (user != null) {
-            if (user.password === password) {
-                res.send({
-                    id: user._id.toString(),
-                    name: user.name
-                })
-
-            } else {
-                res.status(401).send("Não autotizado")
-            }
-        } else {
-            res.status(404).send("Esse usuário não existe. Faça seu cadastro")
+        if (!user) {
+            return res.status(401).send("Não autotizado")
         }
+
+        const passwordOk = bcrypt.compareSync(password, user.password)
+        if (!passwordOk) {
+            return res.status(401).send("Não autorizado")
+        }
+
+        const tokenExist = await db.collection("sessions").findOne({ userId: user._id })
+        if (!tokenExist) {
+            await db.collection("sessions").insertOne({
+                token,
+                userId: user._id
+            })
+            res.send({ token })
+        }
+        res.send({ token: tokenExist.token })
 
 
     } catch (err) {
@@ -77,11 +73,11 @@ app.post("/login", async (req, res) => {
 app.post("/usuarios", async (req, res) => {
     const { name, email, password, confirmedPassword } = req.body
 
-    const validation = validacaoSchema.validate({ email, name }, { abortEarly: false })
+    const validation = validacaoSchema.validate({ email, name, password }, { abortEarly: false })
 
     if (validation.error) {
         const erros = validation.error.details.map((detail) => detail.message)
-        res.status(422).send(erros)
+        res.status(400).send(erros)
         return;
     }
 
@@ -97,9 +93,12 @@ app.post("/usuarios", async (req, res) => {
             res.status(400).send("senhas diferentes")
             return;
         }
+        const hashPassword = bcrypt.hashSync(password, 10)
+        console.log(hashPassword)
 
-        await collectionUser.insertOne({ name, email, password })
+        await collectionUser.insertOne({ name, email, password: hashPassword })
         res.sendStatus(201)
+
 
 
     } catch (error) {
@@ -107,17 +106,15 @@ app.post("/usuarios", async (req, res) => {
         res.sendStatus(500)
     }
 
-
-
-
 })
 
-app.post("/transaction", async (req, res) => {
+app.post("/transaction", async (req, res) => { 
     const { value, description, type } = req.body
-    const { id } = req.headers
-  
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ","")
+    console.log(token)
 
-    const validation = transactionSchema.validate({ value, description, type }, { abortEarly: false })
+   const validation = transactionSchema.validate({ value, description, type }, { abortEarly: false })
 
     if (validation.error) {
         const erros = validation.error.details.map((detail) => detail.message)
@@ -126,14 +123,14 @@ app.post("/transaction", async (req, res) => {
     }
 
     try {
-        const user = await collectionUser.findOne({ _id: ObjectId(id) })
-        if (!user) {
+        const session = await db.collection("sessions").findOne({ token })
+      
+        if (!session) {
             res.send("usuario n existe")
             return;
         }
-
         const transaction = {
-            userId: id,
+            userId:session.userId,
             value: req.body.value,
             description: req.body.description,
             type: req.body.type,
@@ -150,12 +147,31 @@ app.post("/transaction", async (req, res) => {
 })
 
 app.get("/transaction", async (req, res) => {
-    const { id } = req.headers
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ", "")
 
+    if (!token) {
+        return res.sendStatus(401);
+    }
+    
     try {
-        const transactions = await collectionTransaction.find({userId:id}).toArray()
-        res.send(transactions)
-       
+        const sessions = await db.collection("sessions").findOne({ token })
+        const transactions = await collectionTransaction.find({ userId: sessions.userId }).toArray()
+        
+        let total = 0
+        transactions.forEach((transaction)=> {
+            total = total + transaction.value
+        })
+
+        res.send({
+            Transactions:transactions,
+             Total: total
+        })
+
+
+
+        
+
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
